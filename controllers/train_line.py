75812a41c23144a7a3051d27b989e9a4046365ab
shake <hiroki.yamako@ezweb.ne.jp>
2021-11-01 13:37:21 +0900

    [fix]スクレイピング先ページの変更に対応

--- controllers/train_line.py
+++ controllers/train_line.py
@@ -7,13 +7,14 @@ from models.train import TrainLine
 
 
 def fetch_all_names():
-    URL = 'https://transit.yahoo.co.jp/traininfo/area/6/'
+    URL = 'https://transit.yahoo.co.jp/diainfo/area/6'
     html = requests.get(URL)
     soup = BeautifulSoup(html.content, 'html.parser')
-    train_map = soup.find_all('div', class_='elmTblLstLine')
+    train_map = soup.find_all('dd')
+    train_map.pop(0)
     train_name_list = []
     for train in train_map:
-        train_names = train.find_all('a')
+        train_names = train.find_all('dt')
         for train_name in train_names:
             train_name_list.append(train_name.text)
 
